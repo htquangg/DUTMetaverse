5c3e534b2895c71374f6c80db66308ee99ec1fc8
Quang HUYNH <htquangg.dev@gmail.com>
2022-03-27 09:14:49 +0000

    build: add package pm2 to run server

--- Server/dist/src/rooms/commands/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PlayerUpdateCommand = void 0;
-var PlayerUpdateCommand_1 = require("./PlayerUpdateCommand");
-Object.defineProperty(exports, "PlayerUpdateCommand", { enumerable: true, get: function () { return __importDefault(PlayerUpdateCommand_1).default; } });
--- Server/dist/src/types/Room.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RoomState = void 0;
-var RoomState;
-(function (RoomState) {
-    RoomState["LOBBY"] = "lobby";
-    RoomState["PUBLIC"] = "dut";
-    RoomState["CUSTOM"] = "custom";
-})(RoomState = exports.RoomState || (exports.RoomState = {}));
