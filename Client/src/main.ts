7849516d406ba547b667ca583fc04462078235b3
Quang HUYNH <htquangg@gmail.com>
2022-03-28 21:26:15 +0700

    build: add electronjs to build app

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -1,27 +1,68 @@
-import Phaser from 'phaser';
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
 
-import Preload from './scenes/Preload';
-import Game from './scenes/Game';
-import Background from './scenes/Background';
+// Let electron reloads by itself
+if (process.env.ELECTRON_DEBUG === 'true' || process.env.ELECTRON_DEBUG === 'vscode') {
+  // tslint:disable-next-line:no-var-requires
+  require('electron-reload')(__dirname);
+}
 
-import 'regenerator-runtime/runtime'
+let mainWindow: Electron.BrowserWindow | null;
 
-const config: Phaser.Types.Core.GameConfig = {
-  type: Phaser.AUTO,
-  scale: {
-    width: window.innerWidth,
-    height: window.innerHeight,
-    mode: Phaser.Scale.ScaleModes.RESIZE,
-  },
-  pixelArt: true, // Prevent pixel art from becoming blurred when scaled.
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 0 },
-      debug: false,
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
     },
-  },
-  scene: [Preload, Background, Game],
-};
+  });
 
-export default new Phaser.Game(config);
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  if (process.env.ELECTRON_DEBUG === 'true') {
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+  } else if (!process.env.ELECTRON_DEBUG || process.env.ELECTRON_DEBUG === 'false') {
+    // Open window in fullscreen
+    mainWindow.setFullScreen(false);
+    mainWindow.webContents.openDevTools();
+   global.__statics = __dirname
+  }
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it"s common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app"s specific main process
+// code. You can also put them in separate files and require them here.
