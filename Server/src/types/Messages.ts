064618ff02d3cb3507637cfbc2f0f35fcc6eb911
Quang HUYNH <htquangg@gmail.com>
2022-04-08 02:18:09 +0700

    ref(types): enum string messages

--- Server/src/types/Messages.ts
+++ Server/src/types/Messages.ts
@@ -1,5 +1,7 @@
 export enum Messages {
-  UPDATE_PLAYER,
-  READY_TO_CONNECT,
-  SEND_ROOM_DATA,
+  UPDATE_PLAYER = 'update-player',
+  READY_TO_CONNECT = 'ready-to-connect',
+  SEND_ROOM_DATA = 'send-room-data',
+  NEW_COMMER = 'new-commer',
+  USER_LEAVE = 'user-leave'
 }
