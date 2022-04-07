26319db66f9a02ddab570fa652a411bf7b99704f
Quang HUYNH <htquangg@gmail.com>
2022-04-08 02:15:26 +0700

    feat(utils): encrypt/decrypt playerID

--- Client/src/utils/index.ts
+++ Client/src/utils/index.ts
@@ -1,9 +1,25 @@
 export * from './debug';
 export * from './createCustomCursorKeys';
 
+import CryptoJs from 'crypto-js';
+
+const SECRET_KEY = '76S73DGt5WqTYk';
+
 export default class Utils {
   // The ID must start and end with an alphanumeric character (lower or upper case character or a digit). In the middle of the ID spaces, dashes (-) and underscores (_) are allowed.
   static replaceInvalidID(id: string) {
     return id.replace(/[^0-1a-z]/gi, 'G');
+  }
+
+  static encryptID(id: string) {
+    return CryptoJs.AES.encrypt(id, SECRET_KEY);
+  }
+
+  static decryptID(encryptID: string) {
+    return CryptoJs.AES.decrypt(encryptID, SECRET_KEY);
+  }
+
+  static formatEncryptID(id: string): string {
+    return 'cc' + Utils.encryptID(id);
   }
 }
