74bbad713fbd3f1d9621fd3bb714ffc1a14ee483
Quang HUYNH <htquangg@gmail.com>
2022-05-18 01:29:05 +0700

    [Server] test: quick test Data Deletion Request facebook

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import { Server, LobbyRoom } from 'colyseus';
 import cors from 'cors';
-import 'dotenv/config'
+import 'dotenv/config';
+import parser from 'body-parser';
 import { createServer } from 'http';
 import { monitor } from '@colyseus/monitor';
 
@@ -10,10 +11,13 @@ import { RoomState } from './types/';
 
 const port = Number(process.env.PORT) || 3000;
 
-console.log("@@@@@@@@@@@@ env: ", process.env.NODE_ENV)
+console.log('@@@@@@@@@@@@ env: ', process.env.NODE_ENV);
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+//
+// app.use(express.json());
 
 const gameServer = new Server({
   server: createServer(app),
@@ -24,4 +28,16 @@ gameServer.define(RoomState.LOBBY, LobbyRoom);
 gameServer.define(RoomState.PUBLIC, DUTOffice);
 
 app.use('/colyseus', monitor());
+app.post('/fb_data_deletion', (req, res, next) => {
+    console.log(req.body); // {}
+    console.log(req.query); // {}
+
+    if (!req.body || !req.body.signed_request) {
+        console.log('Bad request'); // Ends up here whenever Facebook calls this route
+        // return req.sendStatus(400);
+    }
+
+    // verify request, delete user's data + other code here
+
+});
 gameServer.listen(port);
