e71542fd359f99e665c01d7b7fe0aaad3087425a
Quang HUYNH <htquangg@gmail.com>
2022-06-20 16:55:38 +0700

    [Server] ref: refinding entrypoint

---
+++ Server/src/server.ts
@@ -0,0 +1,39 @@
+import express from 'express';
+import fs from 'fs';
+import { Server, LobbyRoom } from 'colyseus';
+import 'dotenv/config';
+import https from 'https';
+import http from 'http';
+import { DUTOffice } from './rooms/DUTOffice';
+import { RoomState } from './types/';
+import './utils/handleCrash';
+import middleware from './middleware';
+import servicesRoutes from './services';
+import { ServiceConfig } from './config/ServiceConfig';
+import { applyRouteSet, applyMiddleware } from './helper';
+
+const app = express();
+
+applyMiddleware(middleware, app);
+applyRouteSet(servicesRoutes, app);
+
+let gameServer: Server;
+if (ServiceConfig.isDev && ServiceConfig.enableSSL) {
+  const serverOptions = {
+    key: fs.readFileSync(__dirname + '/config/localhost_key.pem'),
+    cert: fs.readFileSync(__dirname + '/config/localhost_cert.pem'),
+  };
+  gameServer = new Server({
+    server: https.createServer(serverOptions, app),
+  });
+} else {
+  gameServer = new Server({
+    server: http.createServer(app),
+  });
+}
+
+// register your room handlers
+gameServer.define(RoomState.LOBBY, LobbyRoom);
+gameServer.define(RoomState.PUBLIC, DUTOffice);
+
+export { gameServer }
