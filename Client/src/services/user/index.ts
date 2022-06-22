14106cf974e6028b0a8cc9eb10c61d00c37aad64
Quang HUYNH <htquangg@gmail.com>
2022-06-22 17:21:09 +0700

    [Client] feat(service): implement api get and update profile

--- Client/src/services/user/index.ts
+++ Client/src/services/user/index.ts
@@ -3,11 +3,10 @@ import axios from 'axios';
 import get from 'lodash/get';
 import { v4 as uuidv4 } from 'uuid';
 
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+
 export const UserService = {
-  getAccessToken: async (
-    { secretKey, playerID, name },
-    callback: (...args: any[]) => void,
-  ) => {
+  getAccessToken: async ({ secretKey, playerID, name }) => {
     try {
       const response = await axiosInstance.post('/gettoken', {
         secretKey,
@@ -15,18 +14,33 @@ export const UserService = {
         name,
       });
 
-      console.log('response: ', response);
-
       const accessToken = get(response, 'data.data.accessToken');
       if (accessToken) {
         axiosInstance.setAuthorizationHeader(accessToken);
-        callback(accessToken);
+        // callback(accessToken);
+        return Promise.resolve(accessToken);
       }
     } catch (error) {
-      await UserService.getAccessToken(
-        { secretKey: uuidv4(), playerID: uuidv4(), name },
-        callback,
-      );
+      await UserService.getAccessToken({
+        secretKey: uuidv4(),
+        playerID: uuidv4(),
+        name,
+      });
     }
+  },
+  getProfile: async ({ playerID }) => {
+    return axiosInstance.get(`http://localhost:3000/player/${playerID}`);
+  },
+  updateProfile: async (data: any) => {
+    const { playerID, ...restData } = data;
+    return axios.patch(`http://localhost:3000/player/${playerID}`, {
+      ...restData,
+    });
+  },
+  updateProfileBeacon: (data: any) => {
+    const blob = new Blob([JSON.stringify(data)], {
+      type: 'application/json',
+    });
+    window.navigator.sendBeacon(`${SERVER_URL}/player/${data.playerID}`, blob);
   },
 };
